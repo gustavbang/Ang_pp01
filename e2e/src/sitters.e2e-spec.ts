c652c1d8505141f49fbbace6b6050514ee1173e8
Gustav Bang <gustavbang96@gmail.com>
2018-10-08 14:24:20 +0200

    Added testing, register functionality and more

--- e2e/src/sitters.e2e-spec.ts
+++ e2e/src/sitters.e2e-spec.ts
@@ -1,25 +1,58 @@
-import { browser, element, by } from 'protractor';
-/*
+import { browser, element, by, $$ } from "protractor";
+import { AppPage } from "./app.po";
 
-1.0:
-
+/* 
+ 1.0: Create a new sitter
 */
 
+
+
 describe('sitters-list', () => {
-    it('1.0: Create a new sitter', () => {
-        browser.get('/login');
 
-        element(by.id('username')).sendKeys('myusername');
-        browser.sleep(1000);
+  // 1: Navigate to findasitter
+  // 2: Fill out login and click
+  // 3: Count number of sitters (a)
+  // 4: Navigate to register without browser.get (no page reload!)
+  // 5: Fill out form to register a new sitter.
+  // 6: Navigate to sitters-list without browser.get (no page reload!)
+  // 7: Count number of sitters (b)
+  // 8: Expect b-a should be 1
 
-        element(by.id('password')).sendKeys('mypassword');
-        browser.sleep(1000);
+  it('1.0: Create a new sitter and after, there should be one more', () => {
+    browser.get('/register2'); // 1
+    let page = new AppPage();
+    //call login
+    page.login();
+browser.sleep(2000);
+console.log("hej");
+    element.all(by.css('.example-card')).then(function(elemsBefore) {
+      // elemsAfter will be a number specifying how many elements of .yourCssClassHere there are in the page.
 
-        element(by.id('btnLogin')).click();
 
+      let sittersCountBefore = elemsBefore.length;
+      element(by.id('menuRegister')).click();
+      // element(by.id('usernameInput')) == $$()
 
-       // expect(true).toBeFalsy();
+      browser.sleep(2000);
+      $$('#usernameInput').sendKeys('Simon');
+      $$('#passwordInput').sendKeys('Simons Password');
+      $$('#nameInput').sendKeys('Simon Ryom');
 
-        browser.sleep(5000);
-    })
+      console.log(elemsBefore.length);
+      $$('#registerSubmit').click();
+      browser.sleep(4000);
+      $$('#username').sendKeys('Simon');
+      $$('#password').sendKeys('Simons Password');
+
+      $$('#btnLogin').click();
+      browser.sleep(2000);
+
+      page.login();
+
+      element.all(by.css('.example-card')).then(function(elemsAfter) {
+        let sittersCountAfter = elemsAfter.length;
+        expect(sittersCountAfter-sittersCountBefore).toBe(1);
+      });
+    });
+  });
 });
--- src/app/register2/register2.component.ts
+++ src/app/register2/register2.component.ts
@@ -1,4 +1,8 @@
+import { TempDataService } from './../temp-data.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Sitter } from '../entities/sitter';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register2',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register2.component.scss']
 })
 export class Register2Component implements OnInit {
+  registerForm;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private data: TempDataService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.registerForm = this.fb.group ( {
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      name: ['', Validators.required],
+
+    })
+  }
+
+  onSubmit(form) {
+    //call an api, to save new sitter
+    let sitter = form.value as Sitter;
+
+    this.data.addSitter(sitter);
+    console.log(this.data.sitters);
+    this.router.navigate(['/login']); 
   }
 
 }
