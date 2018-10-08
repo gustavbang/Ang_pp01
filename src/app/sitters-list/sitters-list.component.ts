042271f0a4b09a558ca50f4031c01390d9779126
Gustav Bang <gustavbang96@gmail.com>
2018-10-08 21:04:32 +0200

    Added edit / delete

--- src/app/sitters-list/sitters-list.component.ts
+++ src/app/sitters-list/sitters-list.component.ts
@@ -1,5 +1,8 @@
 import { TempDataService } from './../temp-data.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Sitter } from '../entities/sitter';
+
+
 
 @Component({
   selector: 'app-sitters-list',
@@ -7,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sitters-list.component.scss']
 })
 export class SittersListComponent implements OnInit {
-
+  @Input() sitter: Sitter;
+  @Output() sitterEditClicked: EventEmitter<any> = new EventEmitter<any>();
   constructor(private tempData: TempDataService) { }
 
   ngOnInit() {
+  }
+
+  onEditClick() {
+    // Handle logic here, or pass event to parent component.
+    this.sitterEditClicked.emit(this.sitter);
   }
 
 }
